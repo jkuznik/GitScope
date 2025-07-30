a187d54f59a58edfcabb13d409700b8efe487fa5
jkuznik <janusz.kuznik89@gmail.com>
2025-07-30 17:36:09 +0200

    Add method to retrieve public repos

--- src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
+++ src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
@@ -10,4 +10,17 @@ public class GitHubClient {
 
     private final RestClient restClient;
 
+    // info source: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
+    public String getReposByUsername(String username) {
+        try {
+            return restClient.get()
+                    .uri("/users/{username}/repos", username)
+                    .header("Accept", "application/vnd.github+json")
+                    .retrieve()
+                    .body(String.class);
+        } catch (Exception e) {
+            log.error("Error fetching repos for user '{}': {}", username, e.getMessage());
+            throw new RuntimeException("Failed to fetch repositories from GitHub", e);
+        }
+    }
 }
