2de9217391bda85350517cc933c73d11f0cc26ca
jkuznik <janusz.kuznik89@gmail.com>
2025-07-30 15:52:23 +0200

    Task start: 30.07.2025

---
+++ src/main/java/pl/jkuznik/gitscope/GitScopeApplication.java
@@ -0,0 +1,13 @@
+package pl.jkuznik.gitscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitScopeApplication.class, args);
+    }
+
+}
