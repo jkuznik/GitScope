9f2a49cb2909c0667d5903e8bcdd4ce058b5cc28
jkuznik <janusz.kuznik89@gmail.com>
2025-07-30 17:45:50 +0200

    Modify getRepos method to allow fetch private repos

--- src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
+++ src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
@@ -2,6 +2,7 @@ package pl.jkuznik.gitscope.infrasctructure.github;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
 
 @Slf4j
@@ -11,11 +12,16 @@ public class GitHubClient {
     private final RestClient restClient;
 
     // info source: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
-    public String getReposByUsername(String username) {
+    public String getReposByUsername(String username, String token) {
         try {
             return restClient.get()
                     .uri("/users/{username}/repos", username)
-                    .header("Accept", "application/vnd.github+json")
+                    .headers(headers -> {
+                        headers.set(HttpHeaders.ACCEPT, "application/vnd.github+json");
+                        if (token != null && !token.isBlank()) {
+                            headers.setBearerAuth(token);
+                        }
+                    })
                     .retrieve()
                     .body(String.class);
         } catch (Exception e) {
