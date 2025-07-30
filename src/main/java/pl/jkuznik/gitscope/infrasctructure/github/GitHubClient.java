fe4e431476d66ced8126b5137abf21570dc95717
jkuznik <janusz.kuznik89@gmail.com>
2025-07-30 22:19:22 +0200

    Update code to allow inspect private repos in case using token

--- src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
+++ src/main/java/pl/jkuznik/gitscope/infrasctructure/github/GitHubClient.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
+import java.util.function.Consumer;
+
 @Slf4j
 @RequiredArgsConstructor
 public class GitHubClient {
@@ -15,21 +17,21 @@ public class GitHubClient {
     // info source: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
     public String getReposByUsername(String username, String token) {
         try {
-            return restClient.get()
-                    .uri("/users/{username}/repos", username)
-                    .headers(headers -> {
-                        headers.set(HttpHeaders.ACCEPT, "application/vnd.github+json");
-                        if (token != null && !token.isBlank()) {
-                            log.info("Token has been include, trying to fetch private repos");
-                            headers.setBearerAuth(token);
-                        } else {
-                            log.info("Token has not been include, trying to fetch public repos");
-                        }
-                    })
-                    .retrieve()
-                    .body(String.class);
+            if (withToken(token)) {
+                return restClient.get()
+                        .uri("/user/repos")
+                        .headers(buildHeaders(token))
+                        .retrieve()
+                        .body(String.class);
+            } else {
+                return restClient.get()
+                        .uri("/users/{username}/repos", username)
+                        .headers(buildHeaders(token))
+                        .retrieve()
+                        .body(String.class);
+            }
         } catch (HttpClientErrorException e) {
-            throw e;
+                throw e;
         } catch (Exception e) {
             log.error("Error fetching repos for user '{}': {}", username, e.getMessage());
             throw new RuntimeException("Failed to fetch repositories from GitHub", e);
@@ -40,12 +42,7 @@ public class GitHubClient {
         try {
             return restClient.get()
                     .uri("/repos/{owner}/{repo}/branches", owner, repo)
-                    .headers(headers -> {
-                        headers.set(HttpHeaders.ACCEPT, "application/vnd.github+json");
-                        if (token != null && !token.isBlank()) {
-                            headers.setBearerAuth(token);
-                        }
-                    })
+                    .headers(buildHeaders(token))
                     .retrieve()
                     .body(String.class);
         } catch (HttpClientErrorException e) {
@@ -54,5 +51,21 @@ public class GitHubClient {
             log.error("Error fetching branches for {}/{}: {}", owner, repo, e.getMessage());
             throw new RuntimeException("Failed to fetch branches from GitHub", e);
         }
+    }
+
+    private Consumer<HttpHeaders> buildHeaders(String token) {
+        return headers -> {
+            headers.set(HttpHeaders.ACCEPT, "application/vnd.github+json");
+            headers.set("X-GitHub-Api-Version", "2022-11-28");
+            if (withToken(token)) {
+                String cleanToken = token.replaceAll("Bearer", "").trim();
+                headers.setBearerAuth(cleanToken);
+                log.debug("Using cleaned token: {}", cleanToken);
+            }
+        };
+    }
+
+    private boolean withToken(String token) {
+        return token != null && !token.isBlank();
     }
 }
--- src/main/java/pl/jkuznik/gitscope/service/GitHubService.java
+++ src/main/java/pl/jkuznik/gitscope/service/GitHubService.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import pl.jkuznik.gitscope.exception.GitHubUserNotFoundException;
 import pl.jkuznik.gitscope.infrasctructure.github.GitHubClient;
 import pl.jkuznik.gitscope.model.github.GitHubBranch;
 import pl.jkuznik.gitscope.model.github.GitHubRepository;
@@ -31,24 +33,40 @@ public class GitHubService {
             for (JsonNode repo : parsedRepos) {
                 if (repo.get("fork").asBoolean()) continue;
 
-                String repoName = repo.get("name").asText();
                 String ownerLogin = repo.get("owner").get("login").asText();
+                if (!ownerLogin.equalsIgnoreCase(username)) continue;
 
-                String branchesAsString = gitHubClient.getBranches(ownerLogin, repoName, token);
-                JsonNode parsedBranches = objectMapper.readTree(branchesAsString);
+                String repoName = repo.get("name").asText();
+                boolean isPrivate = repo.get("private").asBoolean();
 
                 List<GitHubBranch> branches = new ArrayList<>();
-                for (JsonNode branch : parsedBranches) {
-                    String branchName = branch.get("name").asText();
-                    String lastSha = branch.get("commit").get("sha").asText();
+                if (!isPrivate) {
+                    String branchesAsString = gitHubClient.getBranches(ownerLogin, repoName, token);
+                    JsonNode parsedBranches = objectMapper.readTree(branchesAsString);
 
-                    branches.add(new GitHubBranch(branchName, lastSha));
+                    for (JsonNode branch : parsedBranches) {
+                        String branchName = branch.get("name").asText();
+                        String lastSha = branch.get("commit").get("sha").asText();
+
+                        branches.add(new GitHubBranch(branchName, lastSha));
+                    }
+                } else {
+                    branches.add(new GitHubBranch(
+                            "N/A (private repository)",
+                            "Branch details not accessible"
+                    ));
                 }
 
                 result.add(new GitHubRepository(repoName, ownerLogin, branches));
             }
+
+            log.info("Retrieved {} repositories", result.size());
         } catch (HttpClientErrorException e) {
-            throw e;
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new GitHubUserNotFoundException(username);
+            } else {
+                throw e;
+            }
         } catch (Exception e) {
             log.error("Failed to fetch repositories or branches for user '{}'", username, e);
             throw new RuntimeException("Unable to fetch data from GitHub caused unexpected error");
